47c85850dc8c7566268722c0c86ba1c07bb95c0a
Cio-T <cio.xiao.tang@gmail.com>
2014-11-29 11:40:58 -0800

    removed useless helpers

--- router/sr_helper.c
+++ router/sr_helper.c
@@ -15,7 +15,7 @@
 void makeIcmpEchoReply(uint8_t* buf, struct sr_if* out_if){
 	struct sr_ip_hdr *ip_hdr = (struct sr_ip_hdr *)(buf + ETHE_SIZE);
 	struct sr_icmp_hdr * icmp_hdr = (struct sr_icmp_hdr*)(buf + ETHE_SIZE + IP_SIZE);
-	
+
 	ip_hdr->ip_dst = ip_hdr->ip_src;
 	ip_hdr->ip_src = out_if->ip; /* source and destination address */
 
@@ -38,7 +38,7 @@ uint8_t* makeIcmp(uint8_t* buf, struct sr_if* out_if, uint8_t icmp_type, uint8_t
 
 	struct sr_ip_hdr *ip_hdr = (struct sr_ip_hdr *)(new_pac + ETHE_SIZE);
 	struct sr_ip_hdr *ip_hdr_buf = (struct sr_ip_hdr *)(buf + ETHE_SIZE);
-	
+
    	ip_hdr->ip_dst = ip_hdr_buf->ip_src;
 	ip_hdr->ip_src = out_if->ip; /* source and destination address */
    	ip_hdr->ip_p = ip_protocol_icmp;	/* protocol */
@@ -62,56 +62,6 @@ uint8_t* makeIcmp(uint8_t* buf, struct sr_if* out_if, uint8_t icmp_type, uint8_t
 	return new_pac;
 }
 
-void makeAndSendICMP(int len, uint8_t* packet, struct sr_instance* sr, const char* iface,
-		uint8_t icmp_type, uint8_t icmp_code){
-
-	/*packet is alway type IP*/
-	printf("makeAndSendICMP function called\n");
-	uint8_t * buf;
-	buf = (uint8_t *)malloc(len); /*allocate new memory for buf*/
-	memcpy(buf, packet, ETHE_SIZE + IP_SIZE); /*copy the ethernet and ip headers*/
-
-	print_hdrs(buf, len);
-
-	sr_ethernet_hdr_t *ethe_header = (sr_ethernet_hdr_t *)buf;
-	struct sr_ip_hdr *ip_header = (struct sr_ip_hdr *)(buf + ETHE_SIZE);
-	struct sr_icmp_hdr * icmp_header = (struct sr_icmp_hdr*)(buf + ETHE_SIZE + IP_SIZE);
-
-	struct sr_if* out_if = sr_get_interface(sr, iface);
-
-	if (icmp_type == 3) {
-        struct sr_icmp_t3_hdr * icmp3_hdr = (struct sr_icmp_t3_hdr *)icmp_header;
-		memcpy(icmp3_hdr->data, ip_header, ICMP_DATA_SIZE);
-	} else {
-		uint8_t * write_data_to_icmp = buf + ETHE_SIZE + IP_SIZE + ICMP_SIZE;
-		memcpy(write_data_to_icmp, ip_header, ICMP_DATA_SIZE);
-	}
-	prepICMPPacket(icmp_header, icmp_type, icmp_code, len - ETHE_SIZE - IP_SIZE);
-
-	populateIP(ip_header, out_if->ip, len);
-	prepEtheFwd(buf, ((sr_ethernet_hdr_t *)packet)->ether_shost, out_if->addr);
-	ethe_header->ether_type = htons(ethertype_ip);
-
-	printf("***********ICMP packet send back**************");
-	print_hdrs(buf, len);
-
-	if (sr_send_packet(sr, buf, len, iface) < 0)
-		printf("Error sending ICMP packet type %d, code %d\n", icmp_type, icmp_code);
-
-	free(buf);
-}
-
-void prepICMPPacket(struct sr_icmp_hdr * icmp_hdr, int icmp_type, int icmp_code, int size){
-    icmp_hdr->icmp_type = icmp_type;
-    icmp_hdr->icmp_code = icmp_code;
-    if (icmp_type == 3){
-        struct sr_icmp_t3_hdr * icmp3_hdr = (struct sr_icmp_t3_hdr *)icmp_hdr;
-		icmp3_hdr->unused = 0x0;
-		icmp3_hdr->next_mtu = 0x0;
-    }
-    icmp_hdr->icmp_sum = calculate_ICMP_checksum(icmp_hdr, size);
-}
-
 int validateICMPChecksum(struct sr_icmp_hdr* icmp_hdr, int size){
     uint16_t calc_sum = calculate_ICMP_checksum(icmp_hdr, size);
     if (icmp_hdr->icmp_sum == calc_sum){
@@ -120,28 +70,6 @@ int validateICMPChecksum(struct sr_icmp_hdr* icmp_hdr, int size){
     printf("original ICMP sum is %d, and calculated ICMP sum is %d\n",
            icmp_hdr->icmp_sum, calc_sum);
     return 0;
-}
-
-void populateIP(struct sr_ip_hdr* ip_hdr, uint32_t src_ip_addr, int len){
-
-    ip_hdr->ip_hl = 5; /* header length */
-    ip_hdr->ip_v = 4; /*version*/
-    ip_hdr->ip_tos = 0; /* type of service */
-    ip_hdr->ip_len = htons(len - ETHE_SIZE); /* ip data length = total length - ETHE header length*/
-    ip_hdr->ip_off = 0;	/* fragment offset field */
-    ip_hdr->ip_ttl = 64;
-    ip_hdr->ip_id = 0;
-    ip_hdr->ip_p = ip_protocol_icmp;	/* protocol */
-    ip_hdr->ip_dst = ip_hdr->ip_src;
-    ip_hdr->ip_src = src_ip_addr; /* source and destination address */
-    ip_hdr->ip_sum = calculate_IP_checksum(ip_hdr);
-
-}
-
-void prepIpFwd(struct sr_ip_hdr *ip_buf){
-	/*calculate the new ttl and checksum field of ip packet*/
-	--ip_buf->ip_ttl;
-	ip_buf->ip_sum = calculate_IP_checksum(ip_buf);
 }
 
 void prepArpReply(uint8_t *buf){
@@ -203,25 +131,5 @@ void makeAndSendArpReq(unsigned char* dest_mac_addr, uint32_t next_hop_ip,
 
 	free(buf);
 
-}
-
-void prepEtheEchoReply(uint8_t * buf){
-	sr_ethernet_hdr_t *ethe_header = (sr_ethernet_hdr_t *)buf;
-	uint8_t tmp;
-	/*change the src and dest MAC address of buf (prepare to send back)*/
-	int i;
-	for (i=0; i<ETHER_ADDR_LEN; ++i){
-        tmp = ethe_header->ether_shost[i];
-		ethe_header->ether_shost[i] = ethe_header->ether_dhost[i];
-        ethe_header->ether_dhost[i] = tmp;
-	}
-}
-
-
-void revIPPacket(struct sr_ip_hdr* ip_buf){
-    uint32_t tmp;
-    tmp = ip_buf->ip_src;
-    ip_buf->ip_src = ip_buf->ip_dst;
-    ip_buf->ip_dst = tmp;
 }
 
--- router/sr_helper.h
+++ router/sr_helper.h
@@ -15,18 +15,6 @@ void makeIcmpEchoReply(uint8_t* buf, struct sr_if* out_if);
 uint8_t* makeIcmp(uint8_t* buf, struct sr_if* out_if, uint8_t icmp_type, uint8_t icmp_code);
 
 int validateICMPChecksum(struct sr_icmp_hdr* icmp_hdr, int size);
-void prepICMPPacket(struct sr_icmp_hdr*, int, int, int);
-void prepEtheEchoReply(uint8_t*);
-
-/*=helper functions =for making new packets*/
-void makeAndSendICMP(int len, uint8_t* packet, struct sr_instance* sr, const char* iface,
-	uint8_t icmp_type, uint8_t icmp_code);
-void populateICMP(struct sr_icmp_hdr* icmp_head);
-void populateType3ICMP(struct sr_icmp_t3_hdr* icmp3_head);
-void revIPPacket(struct sr_ip_hdr* ip_head);
-
-void populateARP(struct sr_arp_hdr*, unsigned char*, uint32_t, unsigned char*, uint32_t);
-void populateIP(struct sr_ip_hdr*, uint32_t src_ip_addr, int);
 
 /*
 enum pac_len {
